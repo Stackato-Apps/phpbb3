957af789d87aa72b1957b97b8227984d08e54789
Steven Hsu <stevenhsu0@gmail.com>
2012-09-25 15:09:27 -0700

    Fix to keep config file after application updates

---
+++ stackato-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#echo "This scripts does Stackato setup related to filesystem."
+if [ -e "$STACKATO_FILESYSTEM"/config.php ]
+  then
+    echo "Configuration file exists."
+else
+    echo "Configuration file not found. Creating..."
+    touch "$STACKATO_FILESYSTEM"/config.php
+fi
+echo "Cleaning up..."
+ln -s "$STACKATO_FILESYSTEM"/config.php phpBB/config.php
+echo "All Done!"
+
