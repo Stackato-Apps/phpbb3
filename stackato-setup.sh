9834686e61f29c1e11c22eadf4fd3ddaa4995734
Steven Hsu <stevenhsu0@gmail.com>
2012-09-25 17:19:38 -0700

    Fix to delete install folder after application updates

--- stackato-setup.sh
+++ stackato-setup.sh
@@ -2,7 +2,8 @@
 #echo "This scripts does Stackato setup related to filesystem."
 if [ -e "$STACKATO_FILESYSTEM"/config.php ]
   then
-    echo "Configuration file exists."
+    echo "Configuration file exists. Removing installation folder..."
+    rm -rf phpBB/install/
 else
     echo "Configuration file not found. Creating..."
     touch "$STACKATO_FILESYSTEM"/config.php
@@ -10,4 +11,3 @@ fi
 echo "Cleaning up..."
 ln -s "$STACKATO_FILESYSTEM"/config.php phpBB/config.php
 echo "All Done!"
-
